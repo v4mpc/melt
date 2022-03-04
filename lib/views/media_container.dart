a72a9ed08d66b6ba52e9116efc6b4598ec023132
Yona Mahundi <yona101992@gmail.com>
2022-03-04 22:46:32 +0300

    added check box on media container

--- lib/views/media_container.dart
+++ lib/views/media_container.dart
@@ -3,31 +3,41 @@ import 'package:flutter/material.dart';
 
 class MediaContainer extends StatelessWidget {
   final String imagePath;
+  final String mediaDuration;
+  final double borderRadius;
 
-  const MediaContainer({Key? key, required this.imagePath}) : super(key: key);
+  const MediaContainer(
+      {Key? key,
+      required this.imagePath,
+      this.mediaDuration = '',
+      this.borderRadius = 10})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(fit: StackFit.expand,
-        // clipBehavior: Clip.antiAlias,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey.withOpacity(0.5),
-                ),
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage(imagePath),
-                )),
-          ),
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey.withOpacity(0.5),
+              ),
+              borderRadius: BorderRadius.circular(borderRadius),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(imagePath),
+              )),
+        ),
+        if (mediaDuration.isNotEmpty)
           Positioned(
             child: _buildVideoIndicator(context, '0:16'),
             bottom: 5,
             left: 5,
-          )
-        ]);
+          ),
+        _buildSelectionOverlay(context),
+      ],
+    );
   }
 
   Widget _buildVideoIndicator(BuildContext context, String time) {
@@ -55,6 +65,30 @@ class MediaContainer extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildSelectionOverlay(BuildContext context) {
+    return Stack(
+      // fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        Positioned(
+          left: -10,
+          top: -10,
+          child: Checkbox(
+            activeColor: Theme.of(context).primaryColor,
+            shape: const CircleBorder(),
+            value: false,
+            onChanged: (bool? value) {},
+          ),
+        ),
+      ],
     );
   }
 }
